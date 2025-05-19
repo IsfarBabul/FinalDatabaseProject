d9b7606434a419732824c56790de10e7cb869c62
BT_4N2_04 <@>
2025-05-19 14:49:28 -0400

    fixed get_teacher_schedule to take in IDs instead of names

--- main.py
+++ main.py
@@ -28,35 +28,49 @@ def get_student_schedule(student_id):
     return execute_statement(get_database_connection(), statement)
 
 
-def get_teacher_schedule(teacher_name):
-    statement = "CALL Select_Teacher('" + teacher_name + "')"
+def get_teacher_schedule(teacher_id):
+    statement = "CALL Select_Teacher('" + teacher_id + "')"
     return execute_statement(get_database_connection(), statement)
 
+# check if the user inputted a correct identity
+def verify_user(claimed_identity, possible_identities):
+    for possible_identity in possible_identities:
+        if claimed_identity == possible_identity:
+            return True
+    return False
 
-userIdentity = ""
-while userIdentity != "student" and userIdentity != "teacher":
-    userIdentity = input("What is your identity? (teacher or student)")
-    userIdentity = userIdentity.lower()
+# -------MAIN PROJECT--------
+
+
+user_identity = ""
+possible_identities = ["student", "teacher", "administrator"]
+while not verify_user(user_identity, possible_identities):
+    user_identity = input("What is your identity? (teacher, student, or administrator) ")
+    user_identity = user_identity.lower()
     print()
 
-if userIdentity == "student":
-    student_id = input("Enter a student ID: ")
-    results = get_student_schedule(student_id)
+if user_identity == "student" or user_identity == "teacher":
+    id_num = input("Enter your ID: ")
 
-    for result in results:
-        print("Period: ", result[0])
-        print("Course: ", result[1])
-        print("Room: ", result[2])
-        print("Teacher: ", result[3])
+    if user_identity == "student":
+        results = get_student_schedule(id_num)
+
+        for result in results:
+            print("Period: ", result[0])
+            print("Course: ", result[1])
+            print("Room: ", result[2])
+            print("Teacher: ", result[3])
+            print()
+    else:
+        results = get_teacher_schedule(id_num)
         print()
-else:
-    teacher_name = input("Enter a teacher name: ")
-    results = get_teacher_schedule(teacher_name)
-    for result in results:
-        print("Period: ", result[0])
-        print("Course: ", result[1])
-        print("Room: ", result[2])
+        print("Welcome " + results[0][0] + "!")
         print()
+        for result in results:
+            print("Period: ", result[1])
+            print("Course: ", result[2])
+            print("Room: ", result[3])
+            print()
 
 
 
